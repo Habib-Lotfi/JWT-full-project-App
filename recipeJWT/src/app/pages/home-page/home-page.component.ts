6d767af9ba7dd4214d84baf894347065c5c17085
Habib <Habiblotfi@gmail.com>
2024-04-20 14:28:57 -0400

    Step 2: Recipe Form 1

--- recipeJWT/src/app/pages/home-page/home-page.component.ts
+++ recipeJWT/src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateRecipeFormComponent } from '../create-recipe-form/create-recipe-form.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecipeCardComponent],
+  imports: [
+    RecipeCardComponent,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+
+  recipes = [1, 1, 1, 1, 1, 1]
+
+  constructor(public dialog: MatDialog) {}
+
+  handleOpenCreateRecipeForm() {
+    this.dialog.open(CreateRecipeFormComponent)
+  }
 
 }
